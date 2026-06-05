a2170ec71aadd81e7e75bd3ef6441a30bdd421f9
AlmadaBraian <almadabraian93@gmail.com>
2026-06-04 21:30:42 -0300

    Fixes

--- RuntimeCombat.py
+++ RuntimeCombat.py
@@ -6,7 +6,7 @@ import IA
 from RuntimeSkill import RuntimeSkill, update_knockback
 from SpriteManager import Animator
 from config import CAMERA_PRESETS, GRID_H, GRID_W
-from BasicScripts import END_COMBAT_SCRIPT, NORMAL_ATTACK_SCRIPT
+from BasicScripts import NORMAL_ATTACK_SCRIPT
 from SceneManager import get_runtime_scene_manager
 import random
 
